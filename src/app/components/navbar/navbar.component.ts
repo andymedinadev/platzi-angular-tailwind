a68a3166c0eba61506c8e4b975c4ae3cd7226bd3
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-03-18 14:54:39 -0300

    feat: add boards page and navbar component

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { BtnComponent } from '../btn/btn.component';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [BtnComponent],
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent {}
