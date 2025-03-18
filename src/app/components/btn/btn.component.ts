c73082c6806e0b952b578168392495ba7f445918
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-03-18 14:30:41 -0300

    feat: add reusable button component

---
+++ src/app/components/btn/btn.component.ts
@@ -0,0 +1,27 @@
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-btn',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './btn.component.html',
+})
+export class BtnComponent {
+  @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
+  @Input() color: 'success' | 'danger' | 'primary' = 'primary';
+
+  get colors() {
+    return {
+      'bg-success-700': this.color === 'success',
+      'hover:bg-success-800': this.color === 'success',
+      'focus:ring-success-300': this.color === 'success',
+      'bg-primary-700': this.color === 'primary',
+      'hover:bg-primary-800': this.color === 'primary',
+      'focus:ring-primary-300': this.color === 'primary',
+      'bg-red-700': this.color === 'danger',
+      'hover:bg-red-800': this.color === 'danger',
+      'focus:ring-red-300': this.color === 'danger',
+    };
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        success: colors.green,
+        primary: colors.blue
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms')
