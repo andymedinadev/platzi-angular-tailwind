10502edeafb4f335bddade700b833f1419ae2d92
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-03-18 17:47:28 -0300

    feat: enhance boards page with accordion menu

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,22 +1,40 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { NgClass } from '@angular/common';
+
+// ICONS
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
 import {
+  faAngleDown,
+  faAngleUp,
+  faBorderAll,
   faBox,
   faClock,
+  faGear,
+  faHeart,
+  faUsers,
   faWaveSquare,
 } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent, FontAwesomeModule],
+  imports: [NavbarComponent, FontAwesomeModule, CdkAccordionModule, NgClass],
   templateUrl: './boards.component.html',
 })
 export class BoardsComponent {
-  faTrello = faTrello;
-  faBox = faBox;
-  faWaveSquare = faWaveSquare;
-  faClock = faClock;
+  icons = {
+    trello: faTrello,
+    box: faBox,
+    waveSquare: faWaveSquare,
+    clock: faClock,
+    angleUp: faAngleUp,
+    angleDown: faAngleDown,
+    heart: faHeart,
+    borderAll: faBorderAll,
+    users: faUsers,
+    gear: faGear,
+  };
 }
