731e3065d9e40590312ecafedf156b1321d8f868
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-03-18 14:58:58 -0300

    feat: add dropdown overlay menu in navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { OverlayModule } from '@angular/cdk/overlay';
 import { BtnComponent } from '../btn/btn.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [BtnComponent],
+  imports: [BtnComponent, OverlayModule],
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  isOpen = false;
+}
