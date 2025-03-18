8c8f8730777124a0ec38a7708b4a71dd627b58a4
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-03-18 17:25:15 -0300

    feat: add Font Awesome icons in navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { OverlayModule } from '@angular/cdk/overlay';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BtnComponent } from '../btn/btn.component';
+import { faBell, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [BtnComponent, OverlayModule],
+  imports: [BtnComponent, OverlayModule, FontAwesomeModule],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
   isOpen = false;
+  isOpenBody = false;
+
+  faBell = faBell;
+  faInfoCircle = faInfoCircle;
 }
